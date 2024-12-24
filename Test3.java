1e707a7fd68029dd3b36169b2c3ddedf32cfd916
S CHIDAMBARAM <chidambaram176005@gmail.com>
2024-12-24 15:57:27 +0530

    Add files via upload

--- Test3.java
+++ Test3.java
@@ -1,9 +1,37 @@
 package chintu24;
 
+import java.util.Scanner;
+
 public class Test3 {
 
 	public static void main(String[] args) {
 		
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter no of rows");
+		int r=sc.nextInt();
+		System.out.println("enter no of columns");
+		int c=sc.nextInt();
+		int[][] mat=new int[r][c];
+		
+		for(int rows=0;rows<r;rows++) {
+			for(int cols=0;cols<c;cols++) {
+				
+				mat[rows][cols]= sc.nextInt();
+				
+			}
+		//	System.out.println();
+		}
+		//print
+		for(int rows=0;rows<r;rows++) {
+			for(int cols=0;cols<c;cols++) {
+				
+			System.out.print(mat[rows][cols]+" ");
+				
+				
+			}
+			System.out.println();
+		}
 	}
 
 }
