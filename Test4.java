7c904a5f0d1b29bdd6d583a9660d16ed47afaf7b
S CHIDAMBARAM <chidambaram176005@gmail.com>
2024-12-24 15:57:47 +0530

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,27 @@
+package chintu24;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+          
+		
+		int[][] mat= {
+				{1, 2, 3, 44, 5},
+				{3, 4, 5, 6, 9, 77, 88},
+				{6, 7,}
+		        }; 
+		
+		
+		for(int rows=0;rows<3;rows++) {
+			for(int cols=0;cols<mat[rows].length;cols++) {
+				
+				
+				System.out.print(mat[rows][cols]+" ");
+				
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
